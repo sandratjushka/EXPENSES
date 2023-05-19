fb6390436a5c19adf4c04bbe22de36d397b2054a
sandratjushka <72806906+sandratjushka@users.noreply.github.com>
2023-05-19 12:12:00 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,7 +4,7 @@ describe('Expenses App', () => {
   });
 
   it("can find elements", () => {
-    cy.get(".expense-item").should('have.length', 2);
+    cy.get(".expense-item").should('have.length', 0);
     cy.get(".expense-item h2").first().should('have.text', "Kass");
     cy.get(".expense-item h2").last().should("contain.text", "Elevant");
   });
@@ -13,7 +13,7 @@ describe('Expenses App', () => {
     cy.get(".expenses-filter select").select(1);
     cy.get(".expense-item").should("have.length", 2);
     cy.get(".expense-item")
-      .find(".year")
+      .find(".expense-date__year")
       .each((element) => {
         cy.wrap(element).should("contain.text", "2023");
       });
@@ -26,6 +26,6 @@ describe('Expenses App', () => {
     cy.get('form input[type="date"]').type("2023-05-05");
     cy.get('button[type="submit"]').click();
     cy.get(".expense-item").should("have.length", 3);
-    cy.get(".expense-item h2").last().should("have.text", "Mamps");
+    cy.get(".expense-item h2").first().should("have.text", "Mamps");
   });
 });
